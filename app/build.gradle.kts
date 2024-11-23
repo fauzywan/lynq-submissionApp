08fe41ea88d693c32d5f52652acc8ce8890e388c
fauzywan <iwanfauzay46@gmail.com>
2024-11-23 08:44:02 +0700

    add implementation needed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("com.google.devtools.ksp")
+    id("kotlin-parcelize")
 }
 
 android {
@@ -27,11 +29,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
     buildFeatures {
         viewBinding = true
@@ -51,4 +53,26 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation(libs.androidx.activity.ktx)
+    implementation(libs.glide)
+    ksp(libs.room.compiler)
+    //retrofit
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
+    implementation(libs.logging.interceptor)
+
+//    VIEWMODEL
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+//    implementation(libs.lifecycle.livedata.ktx)
+    implementation(libs.androidx.lifecycle.livedata.ktx)
+
+    implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.material3)
+//coroutine support
+    implementation(libs.androidx.room.ktx)
+    implementation(libs.androidx.datastore.preferences)
+//    implementation(libs.androidx.work.runtime)
+    implementation(libs.android.async.http)
+    implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.room.runtime)
 }
